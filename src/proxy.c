47b9ce8856c9f5594fab7562ac42a884c45c3aa9
Claudio Takahasi <ckt@cesar.org.br>
2018-02-21 14:07:28 -0300

    dbus: Add knotd proxy
    This patch adds the proxy abstraction to allow tracking nRF24 devices
    created or removed from knotd service.

--- src/proxy.c
+++ src/proxy.c
@@ -35,6 +35,7 @@
 #define KNOT_DBUS_SERVICE		"br.org.cesar.knot"
 
 static unsigned int watch_id;
+struct l_dbus_client *client;
 
 static void service_appeared(struct l_dbus *dbus, void *user_data)
 {
@@ -46,6 +47,24 @@ static void service_disappeared(struct l_dbus *dbus, void *user_data)
 	hal_log_info("Service disappeared");
 }
 
+static void added(struct l_dbus_proxy *proxy, void *user_data)
+{
+	const char *interface = l_dbus_proxy_get_interface(proxy);
+	const char *path = l_dbus_proxy_get_path(proxy);
+
+	hal_log_info("proxy added: %s %s", path, interface);
+
+	/* Track all devices based on Id property */
+}
+
+static void removed(struct l_dbus_proxy *proxy, void *user_data)
+{
+	hal_log_info("proxy removed: %s %s",
+		     l_dbus_proxy_get_path(proxy),
+		     l_dbus_proxy_get_interface(proxy));
+}
+
+
 int proxy_start(void)
 {
 	watch_id = l_dbus_add_service_watch(dbus_get_bus(), KNOT_DBUS_SERVICE,
@@ -53,11 +72,16 @@ int proxy_start(void)
 						service_disappeared,
 						NULL, NULL);
 
+	client = l_dbus_client_new(dbus_get_bus(), KNOT_DBUS_SERVICE, "/");
+	l_dbus_client_set_proxy_handlers(client, added,
+					 removed, NULL, NULL, NULL);
+
 	return 0;
 }
 
 void proxy_stop(void)
 {
+	l_dbus_client_destroy(client);
 	l_dbus_remove_watch(dbus_get_bus(), watch_id);
 }
 
